014432436ca12e12b5bd5b6c29c2c1db04c1e0e4
zhangyunqing666 <839259612@qq.com>
2019-11-21 20:06:38 +0800

    HW9 UI

---
+++ Homework9/Health Bar/Assets/HPBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class HPBar : MonoBehaviour
+{
+	public Slider mainSlider;
+	public float HP;
+	public float resulthealth;
+	private void Start()
+	{
+		//mainSlider.value = mainSlider.maxValue;
+		resulthealth = HP;
+		mainSlider = GetComponent<Slider>();
+		HP = mainSlider.maxValue;
+	}
+
+	void OnGUI()
+	{
+		if( GUI.Button(new Rect(300,250,80, 40), "加血")) {
+			HP += 10;
+		}
+		if (GUI.Button(new Rect(300, 200, 80, 40), "减血")) {
+			HP -= 10;
+		}
+		resulthealth = Mathf.Lerp(resulthealth, HP, 0.05f);
+		if (resulthealth > 100) resulthealth = 100;
+		else if (resulthealth < 0) resulthealth = 0;
+
+		mainSlider.value = resulthealth;
+	}
+}
---
+++ Homework9/Health Bar/Assets/IMGUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class IMGUI : MonoBehaviour
+{
+	public float HP = 0.0f;
+	private float resultHP;
+
+	private Rect HPBar;
+	private Rect HPUp;
+	private Rect HPDown;
+
+	void Start()
+	{
+		HPBar = new Rect(50, 50, 200, 20);
+		HPUp = new Rect(105, 80, 40, 20);
+		HPDown = new Rect(155, 80, 40, 20);
+		resultHP = HP;
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button(HPUp, "加血"))
+		{
+			resultHP = resultHP + 0.1f > 1.0f ? 1.0f : resultHP + 0.1f;
+		}
+		if (GUI.Button(HPDown, "减血"))
+		{
+			resultHP = resultHP - 0.1f < 0.0f ? 0.0f : resultHP - 0.1f;
+		}
+
+		//插值计算HP值，以实现血条值平滑变化
+		HP = Mathf.Lerp(HP, resultHP, 0.05f);
+
+		// 用水平滚动条的宽度作为血条的显示值
+		GUI.HorizontalScrollbar(HPBar, 0.0f, HP, 0.0f, 1.0f);
+	}
+}
