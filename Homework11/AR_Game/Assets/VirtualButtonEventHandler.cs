3760803f027e775a54c1c5fafee85583af5455e4
zhangyunqing666 <839259612@qq.com>
2019-12-25 19:23:09 +0800

    hw11 AR游戏

---
+++ Homework11/AR_Game/Assets/VirtualButtonEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vb;
+    public Animator ani;
+
+    void Start()
+    {
+        //注册事件处理器
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        //在虚拟按钮中注册TrackableBehaviour事件
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+    
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        ani.SetTrigger("walk");
+        Debug.Log("按钮按下");  
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        ani.SetTrigger("run");
+        Debug.Log("按钮松开");
+    }
+}
